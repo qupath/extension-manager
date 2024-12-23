2757e5ce4367116fa59054b18ab6da9a91048bc4
lleplat <lleplat@ed.ac.uk>
2024-12-23 13:43:52 +0100

    Typo

--- extensionmanager/src/test/java/qupath/ext/extensionmanager/TestUtils.java
+++ extensionmanager/src/test/java/qupath/ext/extensionmanager/TestUtils.java
@@ -9,15 +9,19 @@ import java.util.Collection;
  */
 public class TestUtils {
 
+    private TestUtils() {
+        throw new AssertionError("This class is not instantiable.");
+    }
+
     /**
      * Assert that two lists are equal without taking the order
      * of elements into account.
      * This function doesn't work if some duplicates are present in one
      * of the list.
      *
-     * @param expectedCollection  the expected values
-     * @param actualCollection  the actual values
-     * @param <T>  the type of the elements of the collection
+     * @param expectedCollection the expected values
+     * @param actualCollection the actual values
+     * @param <T> the type of the elements of the collection
      */
     public static <T> void assertCollectionsEqualsWithoutOrder(Collection<? extends T> expectedCollection, Collection<? extends T> actualCollection) {
         Assertions.assertEquals(expectedCollection.size(), actualCollection.size());
