0817ef337d08740190f6c31120a045f858534938
lleplat <lleplat@ed.ac.uk>
2025-01-20 10:46:52 +0000

    Added remove button on catalog manager

--- extensionmanager/src/main/java/qupath/ext/extensionmanager/gui/CatalogManager.java
+++ extensionmanager/src/main/java/qupath/ext/extensionmanager/gui/CatalogManager.java
@@ -5,7 +5,9 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.OverrunStyle;
@@ -60,6 +62,8 @@ class CatalogManager extends Stage {
     private TableColumn<SavedCatalog, URI> urlColumn;
     @FXML
     private TableColumn<SavedCatalog, String> descriptionColumn;
+    @FXML
+    private TableColumn<SavedCatalog, Button> removeColumn;
     @FXML
     private TextField catalogUrl;
 
@@ -197,6 +201,16 @@ class CatalogManager extends Stage {
         nameColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().name()));
         urlColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().uri()));
         descriptionColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().description()));
+        removeColumn.setCellValueFactory(cellData -> {
+            Button button = new Button(resources.getString("CatalogManager.remove"));
+            button.setDisable(!cellData.getValue().deletable());
+            button.setOnAction(event -> {
+                UiUtils.promptExtensionDirectory(extensionCatalogManager.getExtensionDirectoryPath(), onInvalidExtensionDirectory);
+
+                deleteCatalogs(List.of(cellData.getValue()));
+            });
+            return new SimpleObjectProperty<>(button);
+        });
 
         nameColumn.setCellFactory(getStringCellFactory());
         urlColumn.setCellFactory(column -> new TableCell<>() {
@@ -210,12 +224,27 @@ class CatalogManager extends Stage {
                 } else {
                     setTextOverrun(OverrunStyle.LEADING_ELLIPSIS);
                     setText(item.toString());
-
                     setTooltip(new Tooltip(item.toString()));
                 }
             }
         });
         descriptionColumn.setCellFactory(getStringCellFactory());
+        removeColumn.setCellFactory(column -> {
+            TableCell<SavedCatalog, Button> g = new TableCell<>() {
+                @Override
+                protected void updateItem(Button item, boolean empty) {
+                    super.updateItem(item, empty);
+
+                    if (empty || item == null) {
+                        setGraphic(null);
+                    } else {
+                        setGraphic(item);
+                    }
+                }
+            };
+            g.setAlignment(Pos.CENTER);
+            return g;
+        });
     }
 
     private void setDoubleClickHandler() {
@@ -278,49 +307,53 @@ class CatalogManager extends Stage {
                 return;
             }
 
-            List<SavedCatalog> catalogsToDelete = catalogTable.getSelectionModel().getSelectedItems().stream()
-                    .filter(SavedCatalog::deletable)
-                    .toList();
-            if (catalogsToDelete.isEmpty()) {
-                return;
-            }
-
-            boolean deleteExtensions = Dialogs.showConfirmDialog(
-                    resources.getString("CatalogManager.deleteCatalog"),
-                    MessageFormat.format(
-                            resources.getString("CatalogManager.deleteExtensions"),
-                            catalogsToDelete.stream()
-                                    .map(savedCatalog -> {
-                                        try {
-                                            return String.format("\"%s\"", extensionCatalogManager.getCatalogDirectory(savedCatalog));
-                                        } catch (IOException | InvalidPathException | SecurityException | NullPointerException e) {
-                                            logger.error("Cannot retrieve path of {}", savedCatalog.name(), e);
-                                            return null;
-                                        }
-                                    })
-                                    .filter(Objects::nonNull)
-                                    .collect(Collectors.joining("\n"))
-                    )
-            );
-
-            try {
-                extensionCatalogManager.removeCatalogs(
-                        catalogsToDelete,
-                        deleteExtensions
-                );
-            } catch (IOException | SecurityException | NullPointerException e) {
-                logger.error("Error when removing {}", catalogsToDelete, e);
-
-                Dialogs.showErrorMessage(
-                        resources.getString("CatalogManager.error"),
-                        MessageFormat.format(
-                                resources.getString("CatalogManager.cannotRemoveSelectedCatalogs"),
-                                e.getLocalizedMessage()
-                        )
-                );
-            }
+            deleteCatalogs(catalogTable.getSelectionModel().getSelectedItems());
         });
         menu.getItems().add(removeItem);
+    }
+
+    private void deleteCatalogs(List<SavedCatalog> catalogs) {
+        List<SavedCatalog> catalogsToDelete = catalogs.stream()
+                .filter(SavedCatalog::deletable)
+                .toList();
+        if (catalogsToDelete.isEmpty()) {
+            return;
+        }
+
+        boolean deleteExtensions = Dialogs.showConfirmDialog(
+                resources.getString("CatalogManager.deleteCatalog"),
+                MessageFormat.format(
+                        resources.getString("CatalogManager.deleteExtensions"),
+                        catalogsToDelete.stream()
+                                .map(savedCatalog -> {
+                                    try {
+                                        return String.format("\"%s\"", extensionCatalogManager.getCatalogDirectory(savedCatalog));
+                                    } catch (IOException | InvalidPathException | SecurityException | NullPointerException e) {
+                                        logger.error("Cannot retrieve path of {}", savedCatalog.name(), e);
+                                        return null;
+                                    }
+                                })
+                                .filter(Objects::nonNull)
+                                .collect(Collectors.joining("\n"))
+                )
+        );
+
+        try {
+            extensionCatalogManager.removeCatalogs(
+                    catalogsToDelete,
+                    deleteExtensions
+            );
+        } catch (IOException | SecurityException | NullPointerException e) {
+            logger.error("Error when removing {}", catalogsToDelete, e);
+
+            Dialogs.showErrorMessage(
+                    resources.getString("CatalogManager.error"),
+                    MessageFormat.format(
+                            resources.getString("CatalogManager.cannotRemoveSelectedCatalogs"),
+                            e.getLocalizedMessage()
+                    )
+            );
+        }
     }
 
     private static Callback<TableColumn<SavedCatalog, String>, TableCell<SavedCatalog, String>> getStringCellFactory() {
